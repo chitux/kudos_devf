c83dde1d4e0cd9a23d660531d56dfcf7dda2a2b5
chitux <chitux@tux>
2015-10-28 21:16:33 -0600

    pull request observations

--- kudos/company/models.py
+++ kudos/company/models.py
@@ -3,15 +3,15 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=80)
-    email = models.EmailField(max_length=320, uniq=True)
+    email = models.EmailField(max_length=320, unique=True)
     phone = models.CharField(max_length=10)
     invitation_code = models.CharField(max_length=10)
 
 
 class Product(models.Model):
-    company_id = models.ForeingKey(Company)
+    company_id = models.ForeingKey('company.Company')
     name = models.CharField(max_length=80)
     description = models.CharField(max_length=320)
     price = models.FloatField(max_length=100)
-    icon = models.FileField(max_length=10)
+    icon = models.CharField(max_length=320)
 
